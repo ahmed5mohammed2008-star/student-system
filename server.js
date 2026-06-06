a99348fee9042b932d79995b0a837df6c62c6be0
ahmed <ahmed5mohammed2008@gmail.com>
2026-06-07 02:09:41 +0300

    fix railway 502 port issue

--- server.js
+++ server.js
@@ -3,17 +3,21 @@ const path = require("path");
 
 const app = express();
 
+// static files
 app.use(express.static(path.join(__dirname, "public")));
 
+// root
 app.get("/", (req, res) => {
-    res.send("Student System Running 🚀");
+    res.sendFile(path.join(__dirname, "public", "login.html"));
 });
 
+// student page
 app.get("/student", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "student.html"));
 });
 
-const PORT = process.env.PORT || 3000;
+// PORT مهم جدًا
+const PORT = process.env.PORT;
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log("Server running on port " + PORT);
