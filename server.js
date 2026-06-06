9a4bc07ab821fcb28beb5920a63b53a6fad67fd2
ahmed <ahmed5mohammed2008@gmail.com>
2026-06-07 02:02:24 +0300

    fix railway deployment crash

--- server.js
+++ server.js
@@ -3,20 +3,16 @@ const path = require("path");
 
 const app = express();
 
-// لازم public
 app.use(express.static(path.join(__dirname, "public")));
 
-// صفحة رئيسية (مهم جدًا عشان Railway)
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "login.html"));
+    res.send("Student System Running 🚀");
 });
 
-// Student page
 app.get("/student", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "student.html"));
 });
 
-// مهم جدًا Railway
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
